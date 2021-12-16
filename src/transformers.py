24d8a727ae97b92d7fc3319137ccce81f3f9e698
Pierre de Lacaze <delaray@hotmail.com>
2021-12-16 01:06:42 +0100

    Reformatted code.

--- src/transformers.py
+++ src/transformers.py
@@ -1,7 +1,7 @@
-#**************************************************************************
+# *********************************************************************
 # Tranformer Example Using Keras Embedding & Transformer Layers
 #
-#**************************************************************************
+# *********************************************************************
 #
 # Methodology Outline
 # 
@@ -11,33 +11,35 @@
 # ▻ Create classifier model using transformer layer
 # ▻ Train and Evaluate
 #
-#**************************************************************************
+# *********************************************************************
 #
 # Source: https://keras.io/examples/nlp/text_classification_with_transformer/
 #
-#**************************************************************************
+# *********************************************************************
 
-#--------------------------------------------------------------------------
+# ---------------------------------------------------------------------
 # Imports
-#--------------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
 
-#--------------------------------------------------------------------------
+# ---------------------------------------------------------------------
 # Transformer Block
-#--------------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 # Implement a Transformer block as a layer
 
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
         super(TransformerBlock, self).__init__()
-        self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.att = layers.MultiHeadAttention(num_heads=num_heads,
+                                             key_dim=embed_dim)
         self.ffn = keras.Sequential(
-            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
+            [layers.Dense(ff_dim, activation="relu"),
+             layers.Dense(embed_dim),]
         )
         self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
@@ -51,17 +53,20 @@ class TransformerBlock(layers.Layer):
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
-    
-#--------------------------------------------------------------------------
-# Implement embedding layer
-#--------------------------------------------------------------------------
 
-# Two seperate embedding layers, one for tokens, one for token index (positions).
+
+# ---------------------------------------------------------------------
+# Implement embedding layer
+# ---------------------------------------------------------------------
+
+# Two seperate embedding layers, one for tokens, one for token
+# index (positions).
 
 class TokenAndPositionEmbedding(layers.Layer):
     def __init__(self, maxlen, vocab_size, embed_dim):
         super(TokenAndPositionEmbedding, self).__init__()
-        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.token_emb = layers.Embedding(input_dim=vocab_size,
+                                          output_dim=embed_dim)
         self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
 
     def call(self, x):
@@ -70,48 +75,57 @@ class TokenAndPositionEmbedding(layers.Layer):
         positions = self.pos_emb(positions)
         x = self.token_emb(x)
         return x + positions
-        
-#--------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------
 # Download and prepare dataset
-#--------------------------------------------------------------------------
+# ---------------------------------------------------------------------
 
 # Only consider the top 20k words
-vocab_size = 20000
+VOCAB_SIZE = 20000
 
 # Only consider the first 200 words of each movie review
-maxlen = 200
+MAXLEN = 200
 
-def get_and_prepare_data(vocab_size=vocab_size):
-    (x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=vocab_size)
+
+def get_and_prepare_data(vocab_size=VOCAB_SIZE):
+    (x_train, y_train), (x_val, y_val) = \
+        keras.datasets.imdb.load_data(num_words=vocab_size)
     print(len(x_train), "Training sequences")
     print(len(x_val), "Validation sequences")
-    x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
-    x_val = keras.preprocessing.sequence.pad_sequences(x_val, maxlen=maxlen)
+    x_train = \
+        keras.preprocessing.sequence.pad_sequences(x_train, maxlen=MAXLEN)
+    x_val = \
+        keras.preprocessing.sequence.pad_sequences(x_val, maxlen=MAXLEN)
     return x_train, y_train, x_val, y_val
 
-#--------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
 # Create classifier model using transformer layer
-#--------------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 # Transformer layer outputs one vector for each time step of our input
 # sequence. Here, we take the mean across all time steps and use a
 # feed forward network on top of it to classify text.
 
 # Embedding size for each token
-embed_dim = 32
+EMBED_DIM = 32
+
 
 # Number of attention heads
-num_heads = 2
+NUM_HEADS = 2
+
 
 # Hidden layer size in feed forward network inside transformer
-ff_dim = 32  
+FF_DIM = 32  
+
 
 def create_keras_model():
-    inputs = layers.Input(shape=(maxlen,))
-    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+    inputs = layers.Input(shape=(MAXLEN,))
+    embedding_layer = TokenAndPositionEmbedding(MAXLEN, VOCAB_SIZE, EMBED_DIM)
 
     x = embedding_layer(inputs)
-    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    transformer_block = TransformerBlock(EMBED_DIM, NUM_HEADS, FF_DIM)
 
     x = transformer_block(x)
     x = layers.GlobalAveragePooling1D()(x)
@@ -124,19 +138,21 @@ def create_keras_model():
     model = keras.Model(inputs=inputs, outputs=outputs)
 
     return model
-        
-#--------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------
 # Train and Evaluate
-#--------------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 def train_keras_model(model, x_train, y_train, x_val, y_val):
-    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile("adam", "sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
 
     history = model.fit(x_train, y_train, batch_size=32, epochs=2,
                         validation_data=(x_val, y_val))
 
     return history
-  
-#--------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
 # Endo of File
-#--------------------------------------------------------------------------
+# ----------------------------------------------------------------------
